f2bd1b73e94aeae314d05e855cd601b496baf049
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-05-29 15:37:33 -0700

    Add tabulated solution from G4G: add chars of str1 one by one

--- dynamic-programming/edit_distance.py
+++ dynamic-programming/edit_distance.py
@@ -88,8 +88,52 @@ def edit_distance_recursive_improved(s, t, dp):
     return dp[(s, t)]
 
 
+def editDistDP(str1, str2, m, n):
+    """
+    Tabulated solution from Geeks 4 Geeks.
+
+    Idea is to add chars of str1 one by one, and to compute the number of edits needed every time, using the
+        previous results.
+
+    For each new char, either we have a match, or we experiment with the three operations and get the most efficient
+        one
+    """
+    # Create a table to store results of subproblems
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+
+    # Fill d[][] in bottom up manner
+    for i in range(m + 1):
+        for j in range(n + 1):
+
+            # If first string is empty, only option is to
+            # insert all characters of second string
+            if i == 0:
+                dp[i][j] = j  # Min. operations = j
+
+            # If second string is empty, only option is to
+            # remove all characters of second string
+            elif j == 0:
+                dp[i][j] = i  # Min. operations = i
+
+            # If last characters are same, ignore last char
+            # and recur for remaining string
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+
+            # If last character are different, consider all
+            # possibilities and find minimum
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1],  # Insert
+                                   dp[i - 1][j],  # Remove
+                                   dp[i - 1][j - 1])  # Replace
+
+    return dp[m][n]
+
+
 if __name__ == '__main__':
     s = "ecfbefdcfca"
     t = "badfcbebbf"
     operations = editDistance(s=s, t=t)
+    solution_g4g = editDistDP(str1=s, str2=t, m=len(s), n=len(t))
     print("Number of operations: {}.".format(operations))
+    print("G4G tabulated -- Number of operations: {}.".format(solution_g4g))
