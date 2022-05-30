6f162623aa5bd2855828f7a731eb24d5588fa381
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-05-30 14:49:13 -0700

    add buffered solution to tabulate edit distance
    We only need the last row to complete the next one

--- dynamic-programming/edit_distance.py
+++ dynamic-programming/edit_distance.py
@@ -180,6 +180,63 @@ def edit_dist_dp_tabulated_g4g(str1, str2, m, n):
     return dp[m][n]
 
 
+def buffered_tabulated_solution_edit_distance_g4g(str1, str2):
+    """
+    To fill a row in DP array we require only one row the upper row. For example, if we are filling the i = 10 rows in
+    DP array we require only values of 9th row. So we simply create a DP array of 2 x str1 length.
+    """
+    len1 = len(str1)
+    len2 = len(str2)
+
+    # Create a DP array to memoize result
+    # of previous computations
+    DP = [[0 for i in range(len1 + 1)]
+          for j in range(2)]
+
+    # Base condition when second String
+    # is empty then we remove all characters
+    for i in range(0, len1 + 1):
+        DP[0][i] = i
+
+    # Start filling the DP
+    # This loop run for every
+    # character in second String
+    for i in range(1, len2 + 1):
+
+        # This loop compares the char from
+        # second String with first String
+        # characters
+        for j in range(0, len1 + 1):
+
+            # If first String is empty then
+            # we have to perform add character
+            # operation to get second String
+            if (j == 0):
+                DP[i % 2][j] = i
+
+            # If character from both String
+            # is same then we do not perform any
+            # operation . here i % 2 is for bound
+            # the row number.
+            elif (str1[j - 1] == str2[i - 1]):
+                DP[i % 2][j] = DP[(i - 1) % 2][j - 1]
+
+            # If character from both String is
+            # not same then we take the minimum
+            # from three specified operation
+            else:
+                DP[i % 2][j] = (1 + min(DP[(i - 1) % 2][j],
+                                        min(DP[i % 2][j - 1],
+                                            DP[(i - 1) % 2][j - 1])))
+
+            # After complete fill the DP array
+            # if the len2 is even then we end
+            # up in the 0th row else we end up
+            # in the 1th row so we take len2 % 2
+            # to get row
+            return DP[len2 % 2][len1]
+
+
 if __name__ == '__main__':
     s = "ecfbefdcfca"
     t = "badfcbebbf"
@@ -187,3 +244,5 @@ if __name__ == '__main__':
     solution_g4g = edit_dist_dp_tabulated_g4g(str1=s, str2=t, m=len(s), n=len(t))
     print("Number of operations: {}.".format(operations))
     print("G4G tabulated -- Number of operations: {}.".format(solution_g4g))
+    solution_buffered_g4g = buffered_tabulated_solution_edit_distance_g4g(str1=s, str2=t)
+    print("G4G buffered tabulated -- Number of operations: {}.".format(solution_buffered_g4g))
