a6bd74473b94f34ead2e0ef0a24b4d2471871865
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-06-09 18:47:42 -0700

    Correct my solution for rat in maze problem; add G4G solutions

--- backtracking/rat_in_maze_all_solutions.py
+++ backtracking/rat_in_maze_all_solutions.py
@@ -29,33 +29,48 @@ No path exists and destination cell is
 blocked.
 """
 
-""" My solution with backtracking """
-import copy
+""" 
+Prints all possible paths from (0, 0) to (n - 1, n - 1).
 
+Start from the initial index (i.e. (0,0)) and look for the valid moves through the adjacent cells in the order 
+    Down->Left->Right->Up (so as to get the sorted paths) in the grid.
+    
+If the move is possible, then move to that cell while storing the character corresponding to the move(D,L,R,U) and 
+    again start looking for the valid move until the last index (i.e. (n-1,n-1)) is reached.
+Also, keep on marking the cells as visited and when we traversed all the paths possible from that cell, then unmark 
+    that cell for other different paths and remove the character from the path formed.
+As the last index of the grid(bottom right) is reached, then store the traversed path.
+
+Important takeaways:
+    - No need to make deep copies of the map or the visited cells: just edit the parameters as you go through paths,
+        and un-edit them the other way around as you are done with that node
+    - Explore paths in the alphabetical orders to get solutions in the alphabetical order
+ """
+
+# in order for solutions to be in order
 _POSSIBLE_MOVES = {
-    "L",  # left
-    "R",  # right
-    "U",  # up
-    "D",  # down
+    "D",  # left
+    "L",  # right
+    "R",  # up
+    "U",  # down
 }
 
 
 def find_path(m, n):
     possible_paths = set()  # possible solutions
-    possible_paths = _find_path_backtracking(x=0, y=0, move_seq="", m=m, n=n, possible_paths=possible_paths)
+    if _position_is_valid(x=0, y=0, m=m, n=n):
+        possible_paths = _find_path_backtracking(x=0, y=0, move_seq="", m=m, n=n, possible_paths=possible_paths)
     sol = list(possible_paths)
-    sol.sort()
     return sol
 
 
 def _find_path_backtracking(x: int, y: int, move_seq: str, m, n, possible_paths):
     # make new moves if possible...
-    m = copy.deepcopy(m)  # remember where we are -- we don't want a move that will get us right here
-    m[y][x] = 0
+    _cell_is_blocked = (m[y][x] == 0)  # record whether this cell is blocked
+    if not _cell_is_blocked:
+        m[y][x] = 0  # remember we visited this cell -- we don't want a move that will get us right here
 
     for move in _POSSIBLE_MOVES:
-        # we need to keep track of the cells that we visited as we make paths, otherwise there is a risk to
-        #  get stuck doing L - R - L - R, etc.
         x_new, y_new = _make_move(x=x, y=y, move=move)
         move_seq_new = move_seq + move
         if _position_is_valid(x=x_new, y=y_new, m=m, n=n):
@@ -65,8 +80,8 @@ def _find_path_backtracking(x: int, y: int, move_seq: str, m, n, possible_paths)
             else:
                 possible_paths = _find_path_backtracking(x=x_new, y=y_new, move_seq=move_seq_new, m=m, n=n,
                                                          possible_paths=possible_paths)
-
-    # if that position is clearly infeasible, then just give up right here
+    if not _cell_is_blocked:
+        m[y][x] = 1  # re-mark this cell as available for other paths to select
     return possible_paths
 
 
@@ -92,11 +107,133 @@ def _position_is_valid(x: int, y: int, m, n: int) -> bool:
     return True
 
 
+""" G4G solution using "visited" array -- Mark cells as visited as visited as you go, but you don't need to make a
+    deepcopy of the visited matrix every single time: you can just mark the cells as visited as you go along one 
+    path, and then unmark them as visited
+ """
+from typing import List
+
+MAX = 5
+
+
+def is_safe(row: int, col: int, m: List[List[int]], n: int, visited: List[List[bool]]) -> bool:
+    """ Returns True iif the position is valid """
+    return not (row == -1 or row == n or col == -1 or col == n or visited[row][col] or m[row][col] == 0)
+
+
+def print_path_util(row: int, col: int,
+                    m: List[List[int]],
+                    n: int, path: str,
+                    possible_paths: List[str],
+                    visited: List[List[bool]]) -> None:
+
+    # Check the initial point (i.e. (0, 0)) to start the paths
+    if (row == -1 or row == n or
+            col == -1 or col == n or
+            visited[row][col] or m[row][col] == 0):
+        return
+
+    # If reach the last cell (n-1, n-1), then store the path and return
+    if row == n - 1 and col == n - 1:
+        possible_paths.append(path)
+        return
+
+    # Mark the cell as visited
+    visited[row][col] = True
+
+    # Try for all the 4 directions (down, left, right, up) in the given order to get the paths in lexicographical
+    # order
+    if is_safe(row + 1, col, m, n, visited):
+        path += 'D'
+        print_path_util(row + 1, col, m, n, path, possible_paths, visited)
+        path = path[:-1]
+
+    if is_safe(row, col - 1, m, n, visited):
+        path += 'L'
+        print_path_util(row, col - 1, m, n,
+                        path, possible_paths, visited)
+        path = path[:-1]
+
+    if is_safe(row, col + 1, m, n, visited):
+        path += 'R'
+        print_path_util(row, col + 1, m, n,
+                        path, possible_paths, visited)
+        path = path[:-1]
+
+    if is_safe(row - 1, col, m, n, visited):
+        path += 'U'
+        print_path_util(row - 1, col, m, n,
+                        path, possible_paths, visited)
+        path = path[:-1]
+
+    # Mark the cell as unvisited for other possible paths
+    visited[row][col] = False
+
+
+def print_path(m: List[List[int]], n: int):
+    """ Store and print all the valid paths """
+    # vector to store all the possible paths
+    possible_paths = []
+    path = ""
+    visited = [[False for _ in range(MAX)]
+               for _ in range(n)]
+    print_path_util(0, 0, m, n, path,
+                    possible_paths, visited)
+    return possible_paths
+
+
+""" More efficient and memory optimized G4G solution -- Instead of maintaining visited matrix, we can modify the given 
+matrix to treat it as visited matrix. """
+res = []
+
+
+def isValid(row, col, m, n):
+    if (row >= 0 and row < n and col >= 0 and col < n and m[row][col] == 1):
+        return True
+
+    return False
+
+
+def findPathHelper(m, n, x, y, dx, dy, path):
+    global res
+
+    if (x == n - 1 and y == n - 1):
+        res.append(path)
+        return
+
+    dir = "DLRU"
+    for i in range(4):
+        row = x + dx[i]
+        col = y + dy[i]
+        if (isValid(row, col, m, n)):
+            m[row][col] = 2  # used to track visited cells of matrix
+            findPathHelper(m, n, row, col, dx, dy, path + dir[i])
+            m[row][col] = 1  # mark it unvisited yet explorable
+
+
+def findPath(m, n):
+    global res
+
+    res.clear()
+
+    # dx, dy will be used to follow `DLRU` exploring approach
+    # which is lexicographically sorted order
+    dx = [1, 0, 0, -1]
+    dy = [0, -1, 1, 0]
+    if (m[0][0] == 1):
+        m[0][0] = 2
+        findPathHelper(m, n, 0, 0, dx, dy, "")
+
+    return res
+
+
 if __name__ == "__main__":
-    N = 4
+    n = 4
     m = [[1, 0, 0, 0],
          [1, 1, 0, 1],
          [1, 1, 0, 0],
          [0, 1, 1, 1]]
-    my_sol = find_path(m=m, n=N)
-    print(my_sol)
+    my_sol = find_path(m=m, n=n)
+    backtracking_sol = print_path(m=m, n=n)
+    memory_efficient_sol = findPath(m=m, n=n)
+    print("My solution: {}. Backtracking solution: {}.".format(my_sol, backtracking_sol))
