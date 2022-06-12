e38aa2d6e711bb61100565f29115fadb30be1973
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-06-11 22:08:39 -0700

    Add implementation of function to find first solution to n queens

--- backtracking/n_queen_problem.py
+++ backtracking/n_queen_problem.py
@@ -5,6 +5,8 @@ The N queens puzzle is the problem of placing N chess queens on an N×N chessboa
 
 import copy
 
+""" My solution: get all the possible solutions to the N queen problem """
+
 
 def n_queen(n):
     """ Get all possible solutions to N queen problem """
@@ -24,6 +26,7 @@ def recursive_n_queen(n, queen_no, board_attacked, solutions, temp_sol):
     queen_row = board_attacked[queen_no]
     for candidate_col in range(
             n):  # possible to iterate only from queen_no here (symmetry-breaking) and later get all reverted solutions?
+        # can break symmetry by forcing the first queen to be in left side of the board
         if not queen_row[candidate_col]:
             temp_sol.append(candidate_col)
             if queen_no == n - 1:  # base case: all the queens have been placed. We have a solution
@@ -89,5 +92,84 @@ def position_in_board(r, c, n):
     return 0 <= r <= n - 1 and 0 <= c <= n - 1
 
 
+""" Another interesting algorithm: return True if another solution exists """
+
+
+def print_solution(board, n):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=" ")
+        print()
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if a queen can be placed on board[row][col]
+
+    This function is called when "col" queens are already placed in columns from 0 to col - 1, so we only need to
+        check left side for attacking queens
+    """
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nq_dp(board, col, n):
+    # base case: If all queens are placed
+    # then return true
+    if col >= n:
+        return True
+
+    # Consider this column and try placing
+    # this queen in all rows one by one
+    for i in range(n):
+
+        if is_safe(board, i, col, n):
+
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # recur to place rest of the queens
+            if solve_nq_dp(board, col + 1, n) == True:
+                return True
+
+            # If placing queen in board[i][col
+            # doesn't lead to a solution, then
+            # queen from board[i][col]
+            board[i][col] = 0
+
+    # if the queen can not be placed in any row in
+    # this column col then return false
+    return False
+
+
+def solve_nq(n):
+    board = [[0] * n for _ in range(n)]
+    if solve_nq_dp(board, 0, n) == False:
+        print("Solution does not exist")
+        return False
+
+    print_solution(board, n)
+    return True
+
+
 if __name__ == "__main__":
-    my_sol = n_queen(n=4)
+    n = 4
+    my_sol = n_queen(n=n)
+    print("All solutions to N Queens using my solution: {}.".format(my_sol))
+    first_sol = solve_nq(n=n)
