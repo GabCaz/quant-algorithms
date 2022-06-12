a2db7586a26d54104e20a5d9a254132b5eb21896
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-06-11 22:34:30 -0700

    Add pretty G4G solution to getting all solutions to N-Queens

--- backtracking/n_queen_problem.py
+++ backtracking/n_queen_problem.py
@@ -167,9 +167,63 @@ def solve_nq(n):
     print_solution(board, n)
     return True
 
+""" Geeks 4 Geeeks solution to printing all queens  """
+
+
+# Python code to for n Queen placement
+class GfG:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [0] * self.MAX
+        self.no = 0
+
+    def breakLine(self):
+        print("\n------------------------------------------------")
+
+    def canPlace(self, k, i):
+
+        # Helper Function to check
+        # if queen can be placed
+        for j in range(1, k):
+            if (self.arr[j] == i or
+                    (abs(self.arr[j] - i) == abs(j - k))):
+                return False
+        return True
+
+    def display(self, n):
+
+        # Function to display placed queen
+        self.breakLine()
+        self.no += 1
+        print("Arrangement No.", self.no, end=" ")
+        self.breakLine()
+
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if self.arr[i] != j:
+                    print("\t_", end=" ")
+                else:
+                    print("\tQ", end=" ")
+            print()
+
+        self.breakLine()
+
+    def nQueens(self, k, n):
+
+        # Function to check queens placement
+        for i in range(1, n + 1):
+            if self.canPlace(k, i):
+                self.arr[k] = i
+                if k == n:
+                    self.display(n)
+                else:
+                    self.nQueens(k + 1, n)
+
 
 if __name__ == "__main__":
     n = 4
     my_sol = n_queen(n=n)
     print("All solutions to N Queens using my solution: {}.".format(my_sol))
     first_sol = solve_nq(n=n)
+    obj = GfG()
+    obj.nQueens(1, n)
