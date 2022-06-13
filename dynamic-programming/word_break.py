fed9b3e4c9ad28510905ffe262c845d3bca52325
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-06-12 18:13:24 -0700

    Add interesting Leetcode solution using collections.deque

--- dynamic-programming/word_break.py
+++ dynamic-programming/word_break.py
@@ -19,12 +19,15 @@ Note that you are allowed to reuse a dictionary word.
 Example 3:
 
 Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
-Output: false
+Output: false.
+
+Some interesting ideas:
+- collections.deque has O(1) append and pop
 """
 
 from typing import FrozenSet, List
 from functools import lru_cache
-
+from collections import deque
 
 """ My correct solution using memoization, and remembering what indices are feasible """
 
@@ -75,9 +78,34 @@ def word_break_lru_cache(s: str, word_dict: List[str]) -> bool:
     return word_break_memo(s, frozenset(word_dict), 0)
 
 
+""" Leetcode solution using BFS (Breadth-First-Search) """
+
+
+def word_break_bfs(s: str, word_dict: List[str]) -> bool:
+    """
+    Similar to my solution.
+    """
+    word_set = set(word_dict)
+    q = deque()
+    visited = set()
+
+    q.append(0)  # we are starting at 0...
+    while q:
+        start = q.popleft()  # for this possible start that we have...
+        if start in visited:  # if we have already tried this point, then no need to go forward: we know is infeasible
+            continue
+        for end in range(start + 1, len(s) + 1):  # for each possible point we can go to from this start...
+            if s[start:end] in word_set:  # if this point is reachable...
+                q.append(end)  # Then save if. we will be able to start from this end point later on
+                if end == len(s):
+                    return True  # we reached the end of the string!
+        visited.add(start)  # we already tried this start, and starting from this point is infeasible
+    return False
+
+
 if __name__ == "__main__":
     s = "applepenapple"
     word_dict = ["apple", "pen"]
     sol = word_break(s=s, word_dict=word_dict)
     sol_cache = word_break_lru_cache(s=s, word_dict=word_dict)
-
+    sol_bfs = word_break_bfs(s=s, word_dict=word_dict)
