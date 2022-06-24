20e4807ae673a7d1c9f94a68c50546f3d622658a
Gabriel-Cazaubieilh <gaby.cazau@gmail.com>
2022-06-24 09:31:28 -0700

    General Trie

---
+++ data_structures/trie_insert_and_search.py
@@ -0,0 +1,102 @@
+"""
+https://leetcode.com/discuss/interview-question/810154/binary-autocomplete
+
+https://www.geeksforgeeks.org/trie-insert-and-search/
+
+https://www.geeksforgeeks.org/auto-complete-feature-using-trie/
+"""
+
+
+""" Binary auto-complete using Trie """
+
+
+""" General Trie structure """
+
+
+class TrieNode:
+
+    # Trie node class
+    def __init__(self):
+        self.children = [None] * 26
+
+        # isEndOfWord is True if node represent the end of the word
+        self.isEndOfWord = False
+
+
+class Trie:
+
+    # Trie data structure class
+    def __init__(self):
+        self.root = self.get_node()
+
+    def get_node(self):
+
+        # Returns new trie node (initialized to NULLs)
+        return TrieNode()
+
+    def _char_to_index(self, ch):
+
+        # private helper function
+        # Converts key current character into index
+        # use only 'a' through 'z' and lower case
+
+        return ord(ch) - ord('a')
+
+    def insert(self, key):
+
+        # If not present, inserts key into trie
+        # If the key is prefix of trie node,
+        # just marks leaf node
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._char_to_index(key[level])
+
+            # if current character is not present
+            if not pCrawl.children[index]:
+                pCrawl.children[index] = self.get_node()
+            pCrawl = pCrawl.children[index]
+
+        # mark last node as leaf
+        pCrawl.isEndOfWord = True
+
+    def search(self, key):
+
+        # Search key in the trie
+        # Returns true if key presents
+        # in trie, else false
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._char_to_index(key[level])
+            if not pCrawl.children[index]:
+                return False
+            pCrawl = pCrawl.children[index]
+
+        return pCrawl.isEndOfWord
+
+
+# driver function
+def general_trie():
+    # Input keys (use only 'a' through 'z' and lower case)
+    keys = ["the", "a", "there", "anaswe", "any",
+            "by", "their"]
+    output = ["Not present in trie",
+              "Present in trie"]
+
+    # Trie object
+    t = Trie()
+
+    # Construct trie
+    for key in keys:
+        t.insert(key)
+
+    # Search for different keys
+    print("{} ---- {}".format("the", output[t.search("the")]))
+    print("{} ---- {}".format("these", output[t.search("these")]))
+    print("{} ---- {}".format("their", output[t.search("their")]))
+    print("{} ---- {}".format("thaw", output[t.search("thaw")]))
+
+
+if __name__ == '__main__':
+    general_trie()
